7efbcd91e216bf842615f84f27b4482035c61eb4
KeithHello <hongji.li@act1.co.jp>
2023-02-14 14:35:17 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Top level of page
+    </div>
+  )
+}
+
+export default App
